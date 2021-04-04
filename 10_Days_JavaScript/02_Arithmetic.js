80f964d33d402f391693ad7bad13a4f88eb97b22
chaedirdwiantara <chaedirdwiantara@gmail.com>
2021-04-04 19:49:20 +0800

    arithmetic

---
+++ 10_Days_JavaScript/02_Arithmetic.js
@@ -0,0 +1,16 @@
+function getArea(length, width) {
+  let area;
+  // Write your code here
+  console.log(length * width);
+  return area;
+}
+
+getArea(3, 5);
+
+function getPerimeter(length, width) {
+  let perimeter;
+  console.log(2 * (length + width));
+  return perimeter;
+}
+
+getPerimeter(3, 5);
